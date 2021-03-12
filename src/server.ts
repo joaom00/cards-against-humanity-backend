7df67c00e5e8b0ca34c52f5100bc98c38bcbdb92
joaom <joaomljcanal@gmail.com>
2021-03-12 13:19:37 -0300

    deploy config

--- src/server.ts
+++ src/server.ts
@@ -14,12 +14,7 @@ import {
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
+const io = new Server(server);
 
 app.use(cors());
 
